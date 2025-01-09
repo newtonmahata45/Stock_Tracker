0689acff6701c5f476c60dae109bcbcabcd38cbc
Newton Mahata <115547218+newtonmahata45@users.noreply.github.com>
2025-01-09 11:59:40 +0530

    Delete stock-tracker-backend directory

--- stock-tracker-backend/auth-service/src/middlewares/asyncHandler.ts
+++
@@ -1,4 +0,0 @@
-import express from "express"
-export const asyncHandler = (fn: any) => (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
--- stock-tracker-backend/auth-service/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import envConfig from './config/env.config';
-
-const PORT = envConfig.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Auth service running on http://localhost:${PORT}`);
-});
